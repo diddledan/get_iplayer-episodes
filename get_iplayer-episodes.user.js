c7e734bce7448ea9f7b475e09ead40841d5add0f
Daniel Llewellyn <daniel@bowlhat.net>
2020-02-24 19:43:42 +0000

    Rename to include .user in filename
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- get_iplayer-episodes.user.js
+++ get_iplayer-episodes.user.js
@@ -4,8 +4,8 @@
 // @version      0.1
 // @description  Copy all the episodes on a BBC iPlayer season page into the clipboard
 // @author       Daniel Llewellyn <diddledan@gmail.com>
-// @updateURL    https://github.com/diddledan/get_iplayer-episodes/raw/master/src/get_iplayer-episodes.js
-// @downloadURL  https://github.com/diddledan/get_iplayer-episodes/raw/master/src/get_iplayer-episodes.js
+// @updateURL    https://github.com/diddledan/get_iplayer-episodes/raw/master/src/get_iplayer-episodes.user.js
+// @downloadURL  https://github.com/diddledan/get_iplayer-episodes/raw/master/src/get_iplayer-episodes.user.js
 // @match        https://www.bbc.co.uk/iplayer/episodes/*
 // @grant        GM_log
 // @grant        GM_setClipboard
